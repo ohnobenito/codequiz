4c312c379e532d5fb696d98329be95110bafdf19
Benito Gonzalez <ohnobenito@gmail.com>
2020-10-20 10:03:49 -0500

    added some test questions to script - currently about taylor swift, but will eventually be about coding

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,6 +21,41 @@ startButton.addEventListener('click', startQuiz);
 //  WHEN THE GAME ENDS, PLAYER CAN ENTER INITIALS AND SCORE GETS SAVED
 
 
+// LIST OF (test) QUESTIONS
+let questions = [
+    {
+        title: "What was the title of Taylor Swift's first EXCLUSIVELY pop album?",
+        choices: ["reputation", "RED", "1989", "Lover"],
+        answer: "1989"
+    },
+    {
+        title: "During the 2020 pandemic, Taylor released a surprise album. What was it called?",
+        choices: ["woodsong", "folklore", "urban legends", "chromatica"],
+        answer: "folklore"
+    },
+    {
+        title: "When was Taylor Swift born?",
+        choices: ["December 13th, 1989", "February 13th, 1988", "July 13th, 1987", "December 13th, 1988"],
+        answer: "December 13th, 1989"
+    },
+    {
+        title: "Here is another question about",
+        choices: ["this answer", "nah", "okay", "this is the correct choice"],
+        answer: "this is the correct choice"
+    },
+    {
+        title: "sample question",
+        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 3"
+    },
+    {
+        title: "Another Question",
+        choices: ["this is the answer", "nope", "nada", "hmmmm"],
+        answer: "this is the answer"
+    },
+]
+
+
 
 
 
