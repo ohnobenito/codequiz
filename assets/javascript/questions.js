242f45e0e6a16de8effeac6b5959bd982d889a74
Benito Gonzalez <ohnobenito@gmail.com>
2020-10-22 15:16:40 -0500

    moved questions to separate java script

---
+++ assets/javascript/questions.js
@@ -0,0 +1,27 @@
+//Questions to push into empty div
+let questions = [
+    {
+        title: "Who invented Javascript, buddy?",
+        choices: ["Taylor Swift", "Brendan Eich", "David S. Pumpkins", "Steve Jobs"],
+        answer: "Brendan Eich"
+      },
+      {
+        title: "What is NaN short for, pal?",
+        choices: ["Grandma", "No! Almonds! Ever!", "Not A Number", "Never a Noun"],
+        answer: "Not A Number"
+      },
+      {
+        title: "What's the correct syntax to connect your JS file, kid?",
+        choices: ["script src =", "script name =", "script href = ", "script file ="],
+        answer: "script src ="
+      },
+      {
+        title: "What does an alert even do, dude?",
+        choices: ["makes computer scream", "makes a lil pop up", "uh oh, screen goes red", "nothing, I can't get it to work..."],
+        answer: "makes a lil pop up"
+      },{
+        title: "On what page could you change the background color of an html element, bucko?",
+        choices: ["html", "css", "java", "all of 'em"],
+        answer: "all of 'em"
+      },
+];
