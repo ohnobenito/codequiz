cc2d135dda9ec29a96533a89ac0cd0453c83b8ab
Benito Gonzalez <ohnobenito@gmail.com>
2020-10-22 15:17:19 -0500

    added functions to run quiz

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,75 +1,106 @@
 
-//Questions to push into empty div
-let testQuestions = [
-    {
-        question: "Here's a question, Buddy",
-        answers: {
-            a: "this answer",
-            b: "this one",
-            c: "this one tho",
-            d: "alrighty"
-        }, correctAnswer: 'a'
-    },
-    {
-        question: "Here's a question, Buddy",
-        answers: {
-            a: "this answer",
-            b: "this one",
-            c: "this one tho",
-            d: "alrighty"
-        }, correctAnswer: 'c'
-    },
-    {
-        question: "Here's a question, Buddy",
-        answers: {
-            a: "this answer",
-            b: "this one",
-            c: "this one tho",
-            d: "alrighty"
-        }, correctAnswer: 'b'
-    },   
-]
 
-
-testQuestionsIndex = 0;
-timerEl = document.getElementById("time");
-time = testQuestions.length * 15;
+let currentQuestionIndex = 0;
+//TIMER INFO
+let timerEl = document.getElementById("time");
+time = questions.length * 20;
 let timerId;
 
-// WHEN START BUTTON IS CLICKED
-
+//VARIABLES TO REF HTML
+let choicesEl = document.getElementById("choices");
+let questEl = document.getElementById("questions");
 let startButton = document.getElementById('start');
+let quizEl = document.getElementById("quizstart");
+let titleEl = document.getElementById("question-title");
+let endGame = document.getElementById("endgame");
+
+//EVENT LISTENERS
 startButton.addEventListener('click', startQuiz); 
 
-// 1.)  WHEN START BUTTON IS CLICKED:
+//WHEN START BUTTON IS CLICKED START QUIZ FUNCTION STARTS:
+
     function startQuiz() {
-           //HIDE START SCREEN
-       let quizEl = document.getElementById("quizstart");
+           //HIDE THE START SCREEN BY SETTING THE 
        quizEl.setAttribute("class", "hide");
         
-           //REPLACE WITH EMPTY DIV TO PLACE QUESTIONS:
-       let questEl = document.getElementById("questions");
+           //REPLACE WITH EMPTY DIV TO PLACE QUESTIONS BY REMOVING "ID" ATTRIBUTE:
        questEl.removeAttribute("id");
 
-        // TIMER STARTS
+        // TIMER GONNA START NOW
        timerId = setInterval(clockTick, 1000);
-
+        //DISPLAYS TIME
        timerEl.textContent = time;
 
         // LOAD QUESTIONS TO DIV
-         loadQuestions();
+         quizQuestions();
     };
-   
-    // LOAD QUESTIONS TO EMPTY DIV, WITH MULTIPLE CHOICE ANSWERS
-    function loadQuestions() {
-        let titleCard = document.getElementById("question-title");
-        titleCard.innerHTML = "What even is an array, buddy?"
+        // HERE'S THE FUNCTION TO GRAB THE QUESTIONS FROM OUR OTHER JAVA SHEET
 
-        let choicesEL = document.getElementById("answers");
+    function quizQuestions() {
+        // GRABS FIRST (NEXT) QUESTION
+        let currentQuestion = questions[currentQuestionIndex];
+
+        //UPDATE QUESTION TITLE 
+        titleEl.textContent = currentQuestion.title;
        
-        choicesEL.textContent = "could be a thingy!";
-    }
+        // THIS WILL CLEAR OUT ANY OLD ANSWERS
+        choicesEl.innerHTML = "";
 
+        // GRABS THE CHOICES
+        currentQuestion.choices.forEach(function(choice, i) {
+            // ADDS CHOICE BUTTONS FOR ANSWERS
+            let choiceNode = document.createElement("button");
+            choiceNode.setAttribute("class", "choice");
+            choiceNode.setAttribute("value", choice);
+            choiceNode.textContent = i + 1 + ". " + choice;
+
+            //WHEN AN ANSWER IS SELECTED STARTS THIS FUNCTION
+            choiceNode.onclick = questionClick;
+            //APPENDS CHOICE BUTTONS TO CHOICE SECTION ON HTML
+            choicesEl.appendChild(choiceNode);   
+        });
+    };
+
+    function questionClick() {
+        // CHECKS ANSWER
+        if (this.value !== questions[currentQuestionIndex].answer) {
+          // WHEN A QUESTION IS ANSWERED INCORRECTLY, LOSE TIME ON TIMER
+          time -= 10;
+      
+          if (time < 0) {
+            time = 0;
+          }
+      
+          // UPDATES TIMER
+          timerEl.textContent = time;
+        } else {
+            //ADDS 10 SECONDS TO CLOCK IF CORRECT
+            time += 10;
+            // UPDATES TIMER TO REFLECT
+            timerEl.textContent = time;
+        }
+        // WHEN A QUESTION IS ANSWERED A NEW QUESTION POPS UP
+        currentQuestionIndex++;
+      
+        // IF OUT OF QUESTIONS RUN ENDQUIZ FUNCTION
+        if (currentQuestionIndex === questions.length) {
+          quizEnd();
+        }  // IF QUESTIONS AREN'T DONE QUESTIONS CONTINUE
+            else {
+          quizQuestions();
+        }
+      };
+      // FUNCTION TO END GAME
+    function quizEnd() {
+        // HIDE QUESTION SECTION AGAIN
+        questEl.setAttribute("class", "hide");
+        // SHOW HIDDEN ENDGAME DIV
+        endGame.removeAttribute("id");
+        // STOP THE TIMER
+        clearInterval(timerId);
+    };
+
+    // RUNS CLOCK TIMER
     function clockTick() {
         time--;
         timerEl.textContent = time;
@@ -79,17 +110,4 @@ startButton.addEventListener('click', startQuiz);
         }
 
     }
-
-// WHEN A QUESTION IS ANSWERED A NEW QUESTION POPS UP
-
-// WHEN A QUESTION IS ANSWERED INCORRECTLY, LOSE TIME ON TIMER
-
-//  GAME ENDS WHEN TIMER HITS 0 OR ALL QUESTIONS ARE ANSWERED
-
-//  WHEN THE GAME ENDS, PLAYER CAN ENTER INITIALS AND SCORE GETS SAVED
-
-
-
-
-
 
